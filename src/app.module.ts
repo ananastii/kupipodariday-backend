9887e48f4ab0fd6025cd0d8a2098f26b4d79070e
Nastasia <lisitsinan@gmail.com>
2023-08-06 22:14:42 +0300

    refactor: fancy config factory

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,26 +4,14 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import config from './config/config';
-import { Offer } from './offers/entities/offer.entity';
-import { User } from './users/entities/user.entity';
-import { Wish } from './wishes/entities/wish.entity';
-import { Wishlist } from './wishlists/entities/wishlist.entity';
+import { DatabaseConfigFactory } from './config/database.config';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ load: [config], isGlobal: true }),
     TypeOrmModule.forRootAsync({
       inject: [ConfigService],
-      useFactory: async (configService: ConfigService) => ({
-        type: 'postgres',
-        port: configService.get<number>('database.port'),
-        host: configService.get<string>('database.host'),
-        database: configService.get<string>('database.database'),
-        username: configService.get<string>('database.username'),
-        password: configService.get<string>('database.password'),
-        entities: [User, Wish, Wishlist, Offer],
-        synchronize: true,
-      }),
+      useClass: DatabaseConfigFactory,
     }),
   ],
   controllers: [AppController],
--- src/config/config.ts
+++ src/config/config.ts
@@ -1,11 +1,15 @@
+import { join } from 'path';
+
 export default () => ({
   server: { port: parseInt(process.env.SERVER_PORT, 10) || 3001 },
   database: {
+    type: process.env.DATABASE_TYPE || 'postgres',
     host: process.env.DATABASE_HOST || '127.0.0.1',
     port: parseInt(process.env.DATABASE_PORT, 10) || 5432,
     username: process.env.DATABASE_USER || 'student',
     password: process.env.DATABASE_PASSWORD || 'student',
     database: process.env.DATABASE_NAME || 'kupipodariday',
+    entities: [join(__dirname, '/../**/*.entity.{js,ts}')],
     synchronize: Boolean(process.env.SYNCHRONIZE) || true,
   },
 });
