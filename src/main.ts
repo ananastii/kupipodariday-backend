9bd269abbb912a84543c1a34aaa0d5d07bbd1f5f
Nastasia <lisitsinan@gmail.com>
2023-09-04 18:50:50 +0300

    fix: port from config

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,18 @@
 import { ValidationPipe } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { HttpAdapterHost, NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { UnhandledExceptionFilter } from './common/filters/unhandled-exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
+  const configService = app.get(ConfigService);
+  const port = configService.get<number>('server.port');
   const httpAdapterHost = app.get(HttpAdapterHost);
 
   app.useGlobalFilters(new UnhandledExceptionFilter(httpAdapterHost));
   app.useGlobalPipes(new ValidationPipe());
 
-  await app.listen(3000);
+  await app.listen(port);
 }
 bootstrap();
